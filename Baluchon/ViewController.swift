8e42df997f7bbfe6cf2cb57c00b0c07f8dfbc277
xNumero3 <notnumero3@gmail.com>
2023-10-20 16:13:08 +0200

    initial commit

---
+++ Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baluchon
+//
+//  Created by François-Xavier on 20/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
