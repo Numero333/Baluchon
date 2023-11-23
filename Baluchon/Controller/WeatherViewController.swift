ecc2130666e9095a9d63897e7b2b985196c2f437
xNumero3 <notnumero3@gmail.com>
2023-11-23 12:00:46 +0100

    Update logic + Picker + persistancy

--- Baluchon/Controller/WeatherViewController.swift
+++ Baluchon/Controller/WeatherViewController.swift
@@ -7,19 +7,33 @@
 
 import UIKit
 
-final class WeatherViewController: UIViewController, AppServiceDelegate {
+final class WeatherViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, WeatherDelegate {
 
     //MARK: - Property
-    @IBOutlet private var localWeatherTextView: UITextView!
+    @IBOutlet weak var localtempLabel: UILabel!
+    @IBOutlet weak var localInfoLabel: UILabel!
     
+    @IBOutlet weak var distantTempLabel: UILabel!
+    @IBOutlet weak var distantInfoLabel: UILabel!
+    
+    @IBOutlet weak var localImage: UIImageView!
+    @IBOutlet weak var distantImage: UIImageView!
+    
+    @IBOutlet weak var localPicker: UIPickerView!
+    @IBOutlet weak var distantPicker: UIPickerView!
+
     let model = WeatherModel()
-    
+        
     //MARK: - Override
     override func viewDidLoad() {
         super.viewDidLoad()
         model.delegate = self
+        
+        configureLocalPicker()
+        configureDistantPicker()
+        
         Task {
-            await model.getWeather()
+            await model.loadData()
         }
         view.linearGradientBackground()
     }
@@ -31,9 +45,71 @@ final class WeatherViewController: UIViewController, AppServiceDelegate {
         }
     }
     
-    func didUpdate(result: String) {
+    func didUpdateLocalTemp(result: String) {
         DispatchQueue.main.async {
-            self.localWeatherTextView.text = result
+            self.localtempLabel.text = result
         }
+    }
+    
+    func didUpdateLocalInfo(result: String) {
+        DispatchQueue.main.async {
+            self.localInfoLabel.text = result
+        }
+    }
+    
+    func didUpdateDistantTemp(result: String) {
+        DispatchQueue.main.async {
+            self.distantTempLabel.text = result
+        }
+    }
+    
+    func didUpdateDistantInfo(result: String) {
+        DispatchQueue.main.async {
+            self.distantInfoLabel.text = result
+        }
+    }
+    
+    @IBAction func refresh(_ sender: Any) {
+        Task {
+            await self.model.loadData()
+        }
+    }
+    
+    //MARK: - UIPickerDelegate
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return City.allCases.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let city = City.allCases[row]
+        return city.rawValue
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selected = City.allCases[row]
+        switch pickerView.tag {
+        case 1 : self.model.handleCitySelection(city: selected, index: 1, row: row)
+        default: self.model.handleCitySelection(city: selected, index: 0, row: row)
+        }
+        print("did select \(selected)")
+    }
+    
+    //MARK: - Private
+    private func configureLocalPicker() {
+        localPicker.delegate = self
+        localPicker.dataSource = self
+        localPicker.tag = 0
+        localPicker.selectRow(self.model.fromCityRow, inComponent: 0, animated: false)
+    }
+    
+    private func configureDistantPicker() {
+        distantPicker.delegate = self
+        distantPicker.dataSource = self
+        distantPicker.tag = 1
+        distantPicker.selectRow(self.model.toCityRow, inComponent: 0, animated: false)
     }
 }
--- Baluchon/Model/WeatherModel.swift
+++ Baluchon/Model/WeatherModel.swift
@@ -7,20 +7,85 @@
 
 import Foundation
 
+protocol WeatherDelegate: AnyObject {
+    func didFail(error: APIError)
+    func didUpdateLocalTemp(result: String)
+    func didUpdateLocalInfo(result: String)
+    func didUpdateDistantTemp(result: String)
+    func didUpdateDistantInfo(result: String)
+}
+
 final class WeatherModel {
     
-    //MARK: - Accesible
-    weak var delegate: AppServiceDelegate?
+    //MARK: - Property
+    private var cityChoice = UserDefaults.standard
     
-    func getWeather() async {
+    var FromCityLat: String {
+        cityChoice.string(forKey: "FromCityLat") ?? "0.0"
+    }
+    
+    var FromCityLon: String {
+        cityChoice.string(forKey: "FromCityLon") ?? "0.0"
+    }
+    
+    var fromCityRow: Int {
+        cityChoice.integer(forKey: "FromCityRow")
+    }
+    
+    var ToCityLat: String {
+        cityChoice.string(forKey: "ToCityLat") ?? "0.0"
+    }
+    
+    var ToCityLon: String {
+        cityChoice.string(forKey: "ToCityLon") ?? "0.0"
+    }
+    
+    var toCityRow: Int {
+        cityChoice.integer(forKey: "ToCityRow")
+    }
+    
+    //MARK: - Accesible
+    weak var delegate: WeatherDelegate?
+        
+    func loadData() async {
         Task {
-            switch await APIService<WeatherResponse>.performRequest(apiRequest: APIRequest(url: .openWeather, method: .get, parameters: nil)){
+            switch await APIService<WeatherResponse>.performRequest(apiRequest: APIRequest(url: .openWeather, method: .get, parameters: WeatherRequest(latitude: FromCityLat, longitude: FromCityLon).value)){
             case .success(let weather):
-                delegate?.didUpdate(result: weather.main.temp.description)
+                delegate?.didUpdateLocalTemp(result: weather.main.temp.description)
+                delegate?.didUpdateLocalInfo(result: weather.weather[0].description)
             case .failure(let error):
                 delegate?.didFail(error: error)
             }
         }
+        
+        Task {
+            switch await APIService<WeatherResponse>.performRequest(apiRequest: APIRequest(url: .openWeather, method: .get, parameters: WeatherRequest(latitude: ToCityLat, longitude: ToCityLon).value)){
+            case .success(let weather):
+                delegate?.didUpdateDistantTemp(result: weather.main.temp.description)
+                delegate?.didUpdateDistantInfo(result: weather.weather[0].description)
+            case .failure(let error):
+                delegate?.didFail(error: error)
+            }
+        }
+    }
+    
+    func handleCitySelection(city: City, index: Int, row: Int) {
+        if index == 0 {
+            saveCity(value: city.coordinates.latitude, key: "FromCityLat")
+            saveCity(value: city.coordinates.longitude, key: "FromCityLon")
+            cityChoice.set(row, forKey: "FromCityRow")
+            print("saved \(city) with \(city.coordinates.latitude)")
+        } else {
+            saveCity(value: city.coordinates.latitude, key: "ToCityLat")
+            saveCity(value: city.coordinates.longitude, key: "ToCityLon")
+            cityChoice.set(row, forKey: "ToCityRow")
+            print("saved \(city) with \(city.coordinates.latitude)")
+        }
+    }
+        
+    //MARK: - Private
+    private func saveCity(value: String, key: String) {
+        cityChoice.set(value, forKey: key)
     }
 }
 
